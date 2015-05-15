2c3e20d3122a372757a452dc53cc04bc39a0189f
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2015-05-15 17:28:50 +0200

    Add parameters to node gui

--- org.knime.knip.stitching/src/org/knime/knip/stitching/StitchingPlugin.java
+++ org.knime.knip.stitching/src/org/knime/knip/stitching/StitchingPlugin.java
@@ -1,7 +1,6 @@
 package org.knime.knip.stitching;
 
 import ij.ImagePlus;
-import mpicbg.stitching.math.model.TranslationModel2D;
 import mpicbg.stitching.plugins.Stitching_Pairwise;
 import mpicbg.stitching.stitching.StitchingParameters;
 import net.imglib2.type.numeric.RealType;
@@ -24,28 +23,43 @@ public class StitchingPlugin<T extends RealType<T>> implements Command {
     @Parameter(type = ItemIO.INPUT, label = "Image 2")
     private ImagePlus input2;
 
-    @Parameter(type = ItemIO.INPUT, label = "My Threshold")
-    private final double manualThreshold = 50;
+    @Parameter(type = ItemIO.INPUT, label = "Absolute Threshold")
+    private double absoluteThreshold = 5;
+
+    @Parameter(type = ItemIO.INPUT, label = "Check Peaks")
+    private int checkPeaks = 1;
+
+    @Parameter(type = ItemIO.INPUT, label = "Fusion Method")
+    private int fusionMethod = 0;
+
+    @Parameter(type = ItemIO.INPUT, label = "Add Tiles as Rois")
+    private boolean addTilesAsRois = false;
+
+    @Parameter(type = ItemIO.INPUT, label = "Compute Overlap")
+    private boolean computeOverlap = false;
+
+    @Parameter(type = ItemIO.INPUT, label = "Down Sample")
+    private boolean downSample = false;
+
+    @Parameter(type = ItemIO.INPUT, label = "Name of Fused Image")
+    private String fusedName = "Fused";
 
     @Parameter(type = ItemIO.OUTPUT)
     private ImagePlus output;
-
-    TranslationModel2D a = new TranslationModel2D();
 
     @Override
     public void run() {
 
         StitchingParameters params = new StitchingParameters();
-        params.absoluteThreshold = manualThreshold;
-        params.addTilesAsRois = false;
-        params.channel1 = 0;
-        params.channel2 = 0;
-        params.checkPeaks = 1;
-        params.computeOverlap = false;
-        params.displayFusion = true;
-        params.downSample = false;
-        params.fusedName = "derp";
-        params.fusionMethod = 0;
+        params.absoluteThreshold = absoluteThreshold;
+        params.addTilesAsRois = addTilesAsRois;
+        params.checkPeaks = checkPeaks;
+        params.computeOverlap = computeOverlap;
+        // params.displayFusion = true;
+        params.downSample = downSample;
+        params.fusedName = fusedName;
+        params.fusionMethod = fusionMethod;
+        params.dimensionality = 2;
 
         output =
                 Stitching_Pairwise.performPairWiseStitching(input1, input2,
