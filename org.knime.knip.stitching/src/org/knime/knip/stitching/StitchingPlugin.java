7214f5e3a7ee6c3310a46428c2504e418faf0d6f
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2015-05-15 15:51:43 +0200

    reorder and fix project

---
+++ org.knime.knip.stitching/src/org/knime/knip/stitching/StitchingPlugin.java
@@ -0,0 +1,54 @@
+package org.knime.knip.stitching;
+
+import ij.ImagePlus;
+import mpicbg.stitching.math.model.TranslationModel2D;
+import mpicbg.stitching.plugins.Stitching_Pairwise;
+import mpicbg.stitching.stitching.StitchingParameters;
+import net.imglib2.type.numeric.RealType;
+
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.plugin.Menu;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+@Plugin(
+        menu = { @Menu(label = "DeveloperPlugins"), @Menu(label = "Stitching") },
+        description = "Very simple thresholder", headless = true,
+        type = Command.class)
+public class StitchingPlugin<T extends RealType<T>> implements Command {
+
+    @Parameter(type = ItemIO.INPUT, label = "Image 1")
+    private ImagePlus input1;
+
+    @Parameter(type = ItemIO.INPUT, label = "Image 2")
+    private ImagePlus input2;
+
+    @Parameter(type = ItemIO.INPUT, label = "My Threshold")
+    private final double manualThreshold = 50;
+
+    @Parameter(type = ItemIO.OUTPUT)
+    private ImagePlus output;
+
+    TranslationModel2D a = new TranslationModel2D();
+
+    @Override
+    public void run() {
+
+        StitchingParameters params = new StitchingParameters();
+        params.absoluteThreshold = manualThreshold;
+        params.addTilesAsRois = false;
+        params.channel1 = 0;
+        params.channel2 = 0;
+        params.checkPeaks = 1;
+        params.computeOverlap = false;
+        params.displayFusion = true;
+        params.downSample = false;
+        params.fusedName = "derp";
+        params.fusionMethod = 0;
+
+        output =
+                Stitching_Pairwise.performPairWiseStitching(input1, input2,
+                        params);
+    }
+}
