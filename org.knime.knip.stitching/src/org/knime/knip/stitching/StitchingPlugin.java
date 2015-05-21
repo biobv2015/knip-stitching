a66409938f60dbc35e6b0fe4867a33900568a531
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2015-05-21 18:30:46 +0200

    add libs for FFT

--- org.knime.knip.stitching/src/org/knime/knip/stitching/StitchingPlugin.java
+++ org.knime.knip.stitching/src/org/knime/knip/stitching/StitchingPlugin.java
@@ -1,6 +1,7 @@
 package org.knime.knip.stitching;
 
 import ij.ImagePlus;
+import ij.gui.Roi;
 import mpicbg.stitching.plugins.Stitching_Pairwise;
 import mpicbg.stitching.stitching.StitchingParameters;
 import net.imglib2.type.numeric.RealType;
@@ -32,11 +33,17 @@ public class StitchingPlugin<T extends RealType<T>> implements Command {
     @Parameter(type = ItemIO.INPUT, label = "Fusion Method")
     private int fusionMethod = 0;
 
+    @Parameter(type = ItemIO.INPUT, label = "Channel img 1")
+    private int channelImg1 = 2;
+
+    @Parameter(type = ItemIO.INPUT, label = "Channel img 2")
+    private int channelImg2 = 2;
+
     @Parameter(type = ItemIO.INPUT, label = "Add Tiles as Rois")
     private boolean addTilesAsRois = false;
 
     @Parameter(type = ItemIO.INPUT, label = "Compute Overlap")
-    private boolean computeOverlap = false;
+    private boolean computeOverlap = true;
 
     @Parameter(type = ItemIO.INPUT, label = "Down Sample")
     private boolean downSample = false;
@@ -60,9 +67,18 @@ public class StitchingPlugin<T extends RealType<T>> implements Command {
         params.fusedName = fusedName;
         params.fusionMethod = fusionMethod;
         params.dimensionality = 2;
+        params.channel1 = channelImg1;
+        params.channel2 = channelImg2;
+
+        input1.setRoi(createRoi(input1));
+        input2.setRoi(createRoi(input2));
 
         output =
                 Stitching_Pairwise.performPairWiseStitching(input1, input2,
                         params);
+    }
+
+    private Roi createRoi(ImagePlus img) {
+        return new Roi(0, 0, img.getWidth(), img.getHeight());
     }
 }
