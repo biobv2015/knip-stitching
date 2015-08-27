aa67858ecc6b3d2c22c18a24b7603800fbff8226
Eike Heinz <eike.w.heinz@gmail.com>
2015-08-27 14:57:35 +0200

    add test method for min and max fusion

--- org.knime.knip.stitching/test/org/knime/knip/stitching/PairwiseStitchingTest.java
+++ org.knime.knip.stitching/test/org/knime/knip/stitching/PairwiseStitchingTest.java
@@ -18,20 +18,64 @@ public class PairwiseStitchingTest<T extends RealType<T>> extends
     @SuppressWarnings("unchecked")
     @Test
     public void testAverage() throws ImgIOException {
-        StitchingParameters params = new StitchingParameters();
-        params.fusionMethod = FusionType.AVERAGE;
 
         ImgOpener opener = new ImgOpener();
 
-        SCIFIOImgPlus<T> sfimp1 =
+        ImgPlus<T> sfimp1 =
                 (SCIFIOImgPlus<T>) opener.openImgs(
                         "res/testimgs/img3-1.png.ome.tif").get(0);
-        SCIFIOImgPlus<T> sfimp2 =
+        ImgPlus<T> sfimp2 =
                 (SCIFIOImgPlus<T>) opener.openImgs(
                         "res/testimgs/img3-2.png.ome.tif").get(0);
 
         ImgPlus<T> imp1 = ImgPlus.wrap(sfimp1);
         ImgPlus<T> imp2 = ImgPlus.wrap(sfimp2);
+
+        StitchingParameters params = new StitchingParameters();
+        params.fusionMethod = FusionType.AVERAGE;
+
+        PairwiseStitching.performPairWiseStitching(imp1, imp2, params, ops);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testMin() throws ImgIOException {
+
+        ImgOpener opener = new ImgOpener();
+
+        ImgPlus<T> sfimp1 =
+                (SCIFIOImgPlus<T>) opener.openImgs(
+                        "res/testimgs/img3-1.png.ome.tif").get(0);
+        ImgPlus<T> sfimp2 =
+                (SCIFIOImgPlus<T>) opener.openImgs(
+                        "res/testimgs/img3-2.png.ome.tif").get(0);
+
+        ImgPlus<T> imp1 = ImgPlus.wrap(sfimp1);
+        ImgPlus<T> imp2 = ImgPlus.wrap(sfimp2);
+
+        StitchingParameters params = new StitchingParameters();
+        params.fusionMethod = FusionType.MIN_INTENSITY;
+
+        PairwiseStitching.performPairWiseStitching(imp1, imp2, params, ops);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testMax() throws ImgIOException {
+        ImgOpener opener = new ImgOpener();
+
+        ImgPlus<T> sfimp1 =
+                (SCIFIOImgPlus<T>) opener.openImgs(
+                        "res/testimgs/img3-1.png.ome.tif").get(0);
+        ImgPlus<T> sfimp2 =
+                (SCIFIOImgPlus<T>) opener.openImgs(
+                        "res/testimgs/img3-2.png.ome.tif").get(0);
+
+        ImgPlus<T> imp1 = ImgPlus.wrap(sfimp1);
+        ImgPlus<T> imp2 = ImgPlus.wrap(sfimp2);
+
+        StitchingParameters params = new StitchingParameters();
+        params.fusionMethod = FusionType.MAX_INTENSITY;
 
         PairwiseStitching.performPairWiseStitching(imp1, imp2, params, ops);
     }
